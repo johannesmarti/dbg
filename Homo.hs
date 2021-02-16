f81897e0beae5229f157b5e08e2c0dab84dcb626
Johannes Marti <johannes.marti@gmail.com>
2021-02-16 11:03:14 +0100

    memory improvement in allFunctions

--- Homo.hs
+++ Homo.hs
@@ -14,12 +14,14 @@ isHomo :: (Ord x, Ord y) => Function x y -> Graph x -> Graph y -> Bool
 isHomo fct d c =
   assert (dom `isSubsetOf` Graph.domain d) $
   assert (Function.range fct `isSubsetOf` Graph.domain c) $
-  all pairMapsWell product where
+  all pairMapsWell' product where
     dom = Function.domain fct
     product = cartesianProduct labels dom
     pairMapsWell (l,v) = (Set.map (applyFct fct) (successors d l v)) `isSubsetOf`
                                    (successors c l (applyFct fct v))
-
+    pairMapsWell' (l,v) = all succIsWell (successors d l v) where
+                            csuccs = successors c l (applyFct fct v)
+                            succIsWell s = (applyFct fct s) `elem` csuccs
 searchHomos :: (Ord x, Ord y) => Graph x -> Graph y -> [Function x y]
 searchHomos d c = Prelude.filter (\f -> isHomo f d c)
                                  (allFunctions (Graph.domain d) (Graph.domain c))
