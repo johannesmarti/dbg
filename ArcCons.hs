66bf16a0fbef46cdba5f21bc3e2b4af85d666c7f
Johannes Marti <johannes.marti@gmail.com>
2021-02-19 16:54:10 +0100

    performance improvement in ConsSplit homomorphism finder Significant improvement in the handling of the worklist. Whenever the algorithm splits it only restarts arcCons with a worklist containing all arcs to the node that was split on. Before arcCons would be restarted with a complete worklist of all arcs in the graph.

--- ArcCons.hs
+++ ArcCons.hs
@@ -12,41 +12,58 @@ import Graph
 
 type Approx x y = Map x (Set y)
 
-approxToFunction :: (Ord x, Ord y) => Approx x y -> Either (Function x y) [Approx x y]
-approxToFunction apx = worker (Map.toList (Map.map Set.toList apx)) Map.empty where
-  worker [] accum = Left accum
-  worker ((k,[]):_) _ = error "there is an issue"
-  worker ((k,[s]):rest) accum = worker rest (Map.insert k s accum)
-  worker ((k,list):rest) accum = Right (Prelude.map singelize list) where
-    singelize s = Map.insert k (Set.singleton s) (Map.map Set.fromList (Map.fromList rest)) `Map.union` Map.map Set.singleton accum 
+type Worklist x = [(Arc x,Bool)]
+
+completeWorklist :: Ord x => Graph x -> Worklist x
+completeWorklist graph = concatMap (\a -> [(a,True),(a,False)]) (arcs graph)
 
 initialApprox :: Set x -> Set y -> Approx x y
 initialApprox dom codom = Map.fromSet (\_ -> codom) dom
 
+addNodeToWorklist :: Ord x => Graph x -> x -> Worklist x -> Worklist x
+addNodeToWorklist graph node worklist = newItems `Data.List.union` worklist
+  where
+    newItems = concatMap itemsForLabel labels
+    itemsForLabel l =
+      [((node,l,o),False) | o <- Set.toList $ successors graph l node]
+        ++ [((o,l,node),True) | o <- Set.toList $ predecessors graph l node]
+
+
 arcCons :: (Ord x, Ord y) => Graph x -> Graph y -> Approx x y
                                      -> Maybe (Approx x y)
-arcCons d c approx = arcConsWorker initialWorklist approx where
-  initialWorklist = concatMap (\a -> [(a,True),(a,False)]) (arcs d)
+arcCons d c approx = arcConsInner d c (completeWorklist d) approx
+
+arcConsInner :: (Ord x, Ord y) => Graph x -> Graph y -> Worklist x
+                                  -> Approx x y -> Maybe (Approx x y)
+arcConsInner d c worklist approx = arcConsWorker worklist approx where
   arcConsWorker [] apx = Just apx
   arcConsWorker (((a,l,b),checkForward):list) apx = let
     guys = (if checkForward then successors else predecessors) c l
     (next,other) = if checkForward then (a,b) else (b,a)
     (remove,keep) = Set.partition (\candi -> guys candi `disjoint` (apx ! other)) (apx ! next)
     newApx = Map.insert next keep apx
-    toAdd = if Set.null remove
-      then []
-      else concatMap (\l -> [((next,l,o),False) | o <- Set.toList $ successors d l next] ++
-                            [((o,l,next),True) | o <- Set.toList $ predecessors d l next]) labels
+    newWorklist = if Set.null remove
+                    then list
+                    else addNodeToWorklist d next list
       in if Set.null keep
            then Nothing
-           else arcConsWorker (toAdd `Data.List.union` list) newApx
+           else arcConsWorker newWorklist newApx
+
+approxToFunction :: (Ord x, Ord y) => Approx x y -> Either (Function x y) ([Approx x y],x)
+approxToFunction apx = worker (Map.toList (Map.map Set.toList apx)) Map.empty where
+  worker [] accum = Left accum
+  worker ((k,[]):_) _ = error "there is an issue"
+  worker ((k,[s]):rest) accum = worker rest (Map.insert k s accum)
+  worker ((k,list):rest) accum = Right (Prelude.map singelize list,k) where
+    singelize s = Map.insert k (Set.singleton s) (Map.map Set.fromList (Map.fromList rest)) `Map.union` Map.map Set.singleton accum 
 
 arcConsHomos :: (Ord x, Ord y) => Graph x -> Graph y -> [Function x y]
-arcConsHomos d c = worker (initialApprox (Graph.domain d) (Graph.domain c)) where
-  worker apx = do
-    clean <- maybeToList (arcCons d c apx)
-    case approxToFunction clean of
-      Left fct         -> return fct
-      Right splittings -> do
-        split <- splittings
-        worker split
+arcConsHomos d c = let
+    worker worklist apx = do
+      clean <- maybeToList (arcConsInner d c worklist apx)
+      case approxToFunction clean of
+        Left fct                     -> return fct
+        Right (splittings,splitNode) -> do
+          split <- splittings
+          worker (addNodeToWorklist d splitNode []) split
+  in worker (completeWorklist d) (initialApprox (Graph.domain d) (Graph.domain c))
