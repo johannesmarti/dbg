a8bb3e1b56de715a63c147293919e59c9618efe5
Johannes Marti <johannes.marti@gmail.com>
2021-09-20 23:00:31 +0200

    compute infinite elements of Caley graph

--- CaleyGraph.hs
+++ CaleyGraph.hs
@@ -11,9 +11,11 @@ import qualified BitGraph as BG
 import Label
 import UnlabeledBitGraph
 
-newtype CaleyGraph = CaleyGraph {
-  successorMap :: Map.Map Word (Word,Word)
-}
+data CaleyGraph = CaleyGraph {
+  successorMap :: Map.Map Word (Word,Word),
+  -- infiniteElements is the subset of the domain that is hit by infinitely many words over Zero One.
+  infiniteElements :: Set.Set Word
+} deriving Show
 
 domain :: CaleyGraph -> Set Word
 domain = keysSet . successorMap
@@ -26,17 +28,33 @@ succ cg node label = assert (node `elem` domain cg) $
 
 rightCaleyGraph :: Int -> BG.BitGraph -> CaleyGraph
 rightCaleyGraph size bitgraph =
-  CaleyGraph (keepAdding [diagonal size] Map.empty) where
+  CaleyGraph succMap infinites where
     zeroRel = BG.relationOfLabel size bitgraph Zero
     oneRel = BG.relationOfLabel size bitgraph One
-    keepAdding [] m = m
-    keepAdding (next:rest) m = let
+    keepAdding [] m p = (m,p)
+    keepAdding (next:rest) m p = let
       zeroSucc = compose size next zeroRel
       oneSucc = compose size next oneRel
       newM = Map.insert next (zeroSucc,oneSucc) m
+        -- I think the flip in union is potentialy more efficient because Set.union should be used for bigset `union` smallset but insertWith calls the provided function f with f new_value old_value. (This propbabely does not matter for now!)
+      newPred' = Map.insertWith (flip Set.union) zeroSucc (Set.singleton next) p
+      newPred = Map.insertWith (flip Set.union) oneSucc (Set.singleton next) newPred'
       adder elem = if elem `Map.member` newM then id else insertSet elem
       newWorklist = adder zeroSucc $ adder oneSucc rest
-        in keepAdding newWorklist newM
+        in keepAdding newWorklist newM newPred
+    (succMap, predMap) = keepAdding [diagonal size] Map.empty Map.empty
+    computeNonWellfounded [] infinites = infinites
+    computeNonWellfounded (next:rest) infinites = let
+      in if Map.findWithDefault Set.empty next predMap `Set.disjoint` infinites
+           then let (zeroSucc,oneSucc) = succMap ! next
+                    adder elem = if elem `Set.member` infinites
+                                    then insertSet elem
+                                    else id
+                    restPlus = adder zeroSucc $ adder oneSucc rest
+             in computeNonWellfounded restPlus (Set.delete next infinites)
+           else computeNonWellfounded rest infinites
+    infinites = computeNonWellfounded [diagonal size] (keysSet succMap)
+
 
 reflexivityCondition :: Int -> CaleyGraph -> Bool
 reflexivityCondition size cg =
