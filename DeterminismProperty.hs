87f6e36b592b2a988dfb5f749e1adc3d4e15c151
Johannes Marti <johannes.marti@gmail.com>
2021-10-13 08:40:49 +0200

    coding determinism property

--- DeterminismProperty.hs
+++ DeterminismProperty.hs
@@ -1,13 +1,69 @@
 module DeterminismProperty (
   Partition,
+  discrete,
+  isTrivial,
+  representative,
+  eqClass,
+  identify,
   hasDeterminismProperty,
 ) where
 
-import Data.Set
+import Control.Exception
+import Data.Map.Strict as Map
+import Data.Set as Set
 
 import Graph
 
-type Partition = Int
+type Partition a = Map a a
 
-hasDeterminismProperty :: GraphI g x -> g -> Set x -> Maybe Partition
-hasDeterminismProperty = undefined
+discrete :: Set a -> Partition a
+discrete = fromSet id
+
+isTrivial :: Eq a => Partition a -> Bool
+isTrivial partition = Prelude.all (== firstRepresentative) partition where
+  firstRepresentative = snd (Map.findMin partition)
+
+representative :: Ord a => Partition a -> a -> a
+representative partition element = assert (element `Map.member` partition) $
+  partition ! element
+
+eqClass :: Ord a => Partition a -> a -> Set a
+eqClass partition elem = Set.filter sameClass (keysSet partition) where
+  repr = representative partition elem
+  sameClass c = representative partition c == repr
+
+identify :: Ord a => Partition a -> a -> a -> Partition a
+identify partition x y = let
+  repx = representative partition x
+  repy = representative partition y
+  modifier v = if v == repy then repx else v
+    in if repx == repy
+         then partition
+         else Map.map modifier partition
+
+hasDeterminismProperty :: Ord x => GraphI g x -> g -> Set x -> Maybe (Partition x)
+hasDeterminismProperty gi g set = assert (set `isSubsetOf` (domain gi g)) $
+  updatePartition pairs (discrete set) where
+    pairs = ps (Set.toList set)
+    ps [] = []
+    ps (first:others) = Prelude.map (\o -> (first,o)) others ++ ps others
+    updatePartition [] partition = Just partition
+    updatePartition ((x,y):rest) partition =
+      if representative partition x == representative partition y
+          || not (overlappingConstruction x y)
+        then updatePartition rest partition
+      else if identifiable x y partition
+        then let newPartition = identify partition x y
+               in if isTrivial newPartition
+                    then Just newPartition
+                    else updatePartition rest newPartition
+      else Nothing
+    overlappingConstruction x y = all (overlapOnLabel x y) labels
+    overlapOnLabel x y l = not . Set.null $ predecessors gi g l x `Set.intersection` predecessors gi g l y `Set.intersection` set
+    identifiable x y par = all (\node -> (all (ifableOnNodeLabel par x y node)) labels) set
+    ifableOnNodeLabel par x y node l = let
+      preds = predecessors gi g l node
+      xClass = eqClass par x
+      yClass = eqClass par y
+      in (xClass `Set.disjoint` preds && yClass `Set.disjoint` preds)
+           || (xClass `isSubsetOf` preds && yClass `isSubsetOf` preds)
--- Test/DeterminismPropertySpec.hs
+++ Test/DeterminismPropertySpec.hs
@@ -3,6 +3,7 @@ module Test.DeterminismPropertySpec (
 ) where
 
 import Data.Maybe
+import Data.Set
 import Test.Hspec
 
 import DeBruijn
@@ -11,8 +12,28 @@ import Patterns
 import MapGraph
 import DeterminismProperty
 
+
 spec :: Spec
 spec = do
+  describe "partiton is working" $ do
+    let pDis = discrete (fromList [1..6])
+    it "discrete is not trivial" $
+      pDis `shouldSatisfy` (not . isTrivial)
+    let p1 = identify (identify pDis 1 2) 2 3
+    it "identifications are transitive" $
+      representative p1 1 `shouldBe` representative p1 3
+    it "non-identify shold be distinct" $
+      (representative p1 2 == representative p1 4) `shouldBe` False
+    let p2 = identify (identify p1 2 4) 5 6
+    it "classes are the same" $
+      eqClass p2 1 `shouldBe` eqClass p2 4
+    it "classes should stay distict" $
+      (eqClass p2 2 == eqClass p2 5) `shouldBe` False
+    it "with two classes we are not trivial" $
+      p2 `shouldSatisfy` (not . isTrivial)
+    let pt = (identify p2 3 5)
+    it "trivial after identifying everything" $
+      pt `shouldSatisfy` isTrivial
   describe "has Determinism Property" $ do
     it "hamburger" $
       hasDeterminismProperty mapGraphI hamburger (domain mapGraphI hamburger)
