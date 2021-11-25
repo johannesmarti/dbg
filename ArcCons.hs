c41bc60896679e41edfb8937d943284d012af797
Johannes Marti <johannes.marti@gmail.com>
2021-11-25 19:17:25 +0100

    fixing ArcCons

--- ArcCons.hs
+++ ArcCons.hs
@@ -12,7 +12,7 @@ import Data.List
 import Data.Maybe (maybeToList)
 
 import Function
-import Graph
+import LabeledGraph
 import Homo
 
 type Approx x y = Map x (Set y)
@@ -30,10 +30,10 @@ readOffFunction = Map.map (Set.elemAt 0)
 
 type Worklist x = [(Arc x,Bool)]
 
-completeWorklist :: Ord x => GraphI g x -> g -> Worklist x
+completeWorklist :: Ord x => LabeledGraphI g x -> g -> Worklist x
 completeWorklist gi graph = concatMap (\a -> [(a,True),(a,False)]) (arcs gi graph)
 
-addNodeToWorklist :: Ord x => GraphI g x -> g -> x -> Worklist x -> Worklist x
+addNodeToWorklist :: Ord x => LabeledGraphI g x -> g -> x -> Worklist x -> Worklist x
 addNodeToWorklist gi graph node worklist = newItems `Data.List.union` worklist
   where
     newItems = concatMap itemsForLabel labels
@@ -42,11 +42,11 @@ addNodeToWorklist gi graph node worklist = newItems `Data.List.union` worklist
         ++ [((o,l,node),True) | o <- Set.toList $ predecessors gi graph l node]
 
 
-arcCons :: (Ord x, Ord y) => GraphI g1 x -> GraphI g2 y -> g1 -> g2
+arcCons :: (Ord x, Ord y) => LabeledGraphI g1 x -> LabeledGraphI g2 y -> g1 -> g2
                                      -> Approx x y -> Maybe (Approx x y)
 arcCons di ci d c approx = arcConsInner di ci d c (completeWorklist di d) approx
 
-arcConsInner :: (Ord x, Ord y) => GraphI g1 x -> GraphI g2 y -> g1 -> g2 -> Worklist x
+arcConsInner :: (Ord x, Ord y) => LabeledGraphI g1 x -> LabeledGraphI g2 y -> g1 -> g2 -> Worklist x
                                   -> Approx x y -> Maybe (Approx x y)
 arcConsInner di ci d c worklist approx = arcConsWorker worklist approx where
   arcConsWorker [] apx = Just apx
@@ -74,7 +74,7 @@ splittingsAt approx node = let
   in Prelude.map singlified valueList
 
 
-arcConsHomosFromApprox :: (Ord x, Ord y) => GraphI g1 x -> GraphI g2 y
+arcConsHomosFromApprox :: (Ord x, Ord y) => LabeledGraphI g1 x -> LabeledGraphI g2 y
                             -> Approx x y -> HomoSearch g1 g2 x y
 arcConsHomosFromApprox di ci approx d c = let
     worker worklist apx = do
@@ -86,7 +86,7 @@ arcConsHomosFromApprox di ci approx d c = let
                     worker (addNodeToWorklist di d splitNode []) split
   in worker (completeWorklist di d) approx
 
-arcConsHomos :: (Ord x, Ord y) => GraphI g1 x -> GraphI g2 y -> HomoSearch g1 g2 x y
+arcConsHomos :: (Ord x, Ord y) => LabeledGraphI g1 x -> LabeledGraphI g2 y -> HomoSearch g1 g2 x y
 arcConsHomos di ci d c = arcConsHomosFromApprox di ci
-  (fullApprox (Graph.domain di d) (Graph.domain ci c)) d c
+  (fullApprox (LabeledGraph.domain di d) (LabeledGraph.domain ci c)) d c
 
--- Homo.hs
+++ Homo.hs
@@ -10,14 +10,14 @@ import Data.Map.Strict as Map
 import Data.Set as Set
 
 import Function
-import Graph
+import LabeledGraph
 
 type HomoSearch g1 g2 x y = g1 -> g2 -> [Function x y]
 
-isHomo :: (Ord x, Ord y) => GraphI g1 x -> GraphI g2 y -> Function x y -> g1 -> g2 -> Bool
+isHomo :: (Ord x, Ord y) => LabeledGraphI g1 x -> LabeledGraphI g2 y -> Function x y -> g1 -> g2 -> Bool
 isHomo di ci fct d c =
-  assert (dom `isSubsetOf` Graph.domain di d) $
-  assert (Function.range fct `isSubsetOf` Graph.domain ci c) $
+  assert (dom `isSubsetOf` LabeledGraph.domain di d) $
+  assert (Function.range fct `isSubsetOf` LabeledGraph.domain ci c) $
   all pairMapsWell' product where
     dom = Function.domain fct
     product = cartesianProduct labels dom
@@ -27,9 +27,9 @@ isHomo di ci fct d c =
                             csuccs = successors ci c l (applyFct fct v)
                             succIsWell s = (applyFct fct s) `elem` csuccs
 
-searchHomos :: (Ord x, Ord y) => GraphI g1 x -> GraphI g2 y -> HomoSearch g1 g2 x y
+searchHomos :: (Ord x, Ord y) => LabeledGraphI g1 x -> LabeledGraphI g2 y -> HomoSearch g1 g2 x y
 searchHomos di ci d c = Prelude.filter (\f -> isHomo di ci f d c)
-                                 (allFunctions (Graph.domain di d) (Graph.domain ci c))
+                                 (allFunctions (LabeledGraph.domain di d) (LabeledGraph.domain ci c))
 
 noHomo :: HomoSearch g1 g2 x y -> g1 -> g2 -> Bool
 noHomo search d c = Prelude.null $ search d c
