c0e0f7bf327463fdb70058998fce9204b54f881e
Johannes Marti <johannes.marti@gmail.com>
2022-03-14 11:14:59 +0100

    implementing WordTree

---
+++ WordTree.hs
@@ -0,0 +1,53 @@
+module WordTree (
+  WordTree,
+  wordTree,
+) where
+
+import Control.Exception.Base
+import qualified Data.Map.Strict as Map
+import qualified Data.Set as Set
+
+import Debug.Trace
+
+import Label
+import BitGraph
+
+data WordTreeGenerator d = WordTreeGenerator {
+  start :: d,
+  appendZero :: d -> d,
+  appendOne :: d -> d
+}
+
+data WordTree d = WordTreeNode {
+  label    :: d,
+  zeroSucc :: WordTree d,
+  oneSucc  :: WordTree d
+}
+
+wordTree :: WordTreeGenerator d -> WordTree d
+wordTree di = worker (start di) where
+  worker d = WordTreeNode d
+               (worker (appendZero di d))
+               (worker (appendOne  di d))
+
+-- This function could maybe be made quicker by using Sequence.
+allWordsUntil :: WordTree d -> (d -> Bool) -> [([Label],WordTree d)]
+allWordsUntil wt untilCondition = generateAllWords [([],wt)] where
+  generateAllWords [] = []
+  generateAllWords ((nextWord, nextTree):rest) =
+    if untilCondition (label nextTree)
+      then (reverse nextWord,nextTree) : generateAllWords rest
+      else (reverse nextWord,nextTree) :
+                 (generateAllWords (rest ++ [(Zero:nextWord,zeroSucc nextTree),
+                                             (One:nextWord,oneSucc nextTree)]))
+
+-- This function could maybe be made quicker by using Sequence.
+allWordsWithout :: WordTree d -> (d -> Bool) -> [([Label],WordTree d)]
+allWordsWithout wt withoutCondition = generateAllWords [([], wt)] where
+  generateAllWords [] = []
+  generateAllWords ((nextWord, nextTree):rest) =
+    if withoutCondition (label nextTree)
+      then generateAllWords rest
+      else (reverse nextWord,nextTree) :
+                 (generateAllWords (rest ++ [(Zero:nextWord,zeroSucc nextTree),
+                                             (One:nextWord,oneSucc nextTree)]))
