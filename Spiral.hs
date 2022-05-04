d4a6482f05cad833c209d86406b5d5a18a62dba0
Johannes Marti <johannes.marti@gmail.com>
2022-05-04 19:05:35 +0200

    can compute spirals

--- Spiral.hs
+++ Spiral.hs
@@ -5,6 +5,8 @@ module Spiral (
 import Control.Exception.Base
 import qualified Data.Vector as Vec
 import qualified Data.Map as Map
+import qualified Data.Set as Set
+import qualified Data.Set.Extra as SE
 
 import Label
 import LabeledGraph
@@ -13,7 +15,7 @@ data Spiral a = Spiral {
   word   :: Vec.Vector Label,
   hub    :: Vec.Vector a,
   spokes :: Vec.Vector (Map.Map a Int)
-} deriving Eq
+} deriving (Eq, Show)
 
 allIndices :: Vec.Vector a -> [Int]
 allIndices v = [0 .. Vec.length v - 1]
@@ -37,10 +39,33 @@ fromHub :: Ord a => Ord a => LabeledGraphI g a -> g
                              -> [Label] -> [a] -> Spiral a
 fromHub gi g w hubList = assert (length w == length hubList) $ 
                          assert (hubIsConnected gi g w hubList) $
-                         Spiral rword rhub
-                             (atDistance 1 (spiralOnHub word hub)) where
+                         assert (isCoherent spiral) $
+                         spiral where
+  spiral = Spiral rword rhub (atDistance 1 initialGenerator spokesAtHub)
   rword = Vec.fromList w
   rhub = Vec.fromList hubList
-  spiralOnHub = undefined
-  atDistance depth h = undefined
+  sz = Vec.length rword
+  prevIndex i = (sz + i - 1) `mod` sz
+  nextIndex i = (i + 1) `mod` sz
+  spokesAtHub = Vec.generate sz (\i -> Map.singleton (rhub Vec.! i) 0)
+  initialGenerator = Vec.generate sz (\i -> Set.singleton (rhub Vec.! i))
+  atDistance distance generator spokesAccum =
+    if all Set.null generator
+      then spokesAccum
+      else let a = undefined
+               newGenerator = Vec.generate sz (\i -> let
+                   pi = prevIndex i
+                   l = rword Vec.! pi
+                   alreadyThere = Map.keysSet (spokesAccum Vec.! i)
+                   -- probabely it would be more efficient to already remove
+                   -- the elements from alreadyThere from the successors before
+                   -- we do the concatMap
+                   allCandidates = SE.concatMap (successors gi g l)
+                                     (generator Vec.! pi)
+                 in allCandidates Set.\\ alreadyThere)
+               newSpokes = Vec.generate sz (\i ->
+                    Map.union
+                      (spokesAccum Vec.! i)
+                      (Map.fromSet (const distance) (newGenerator Vec.! i)))
+             in atDistance (distance + 1) newGenerator newSpokes
 
