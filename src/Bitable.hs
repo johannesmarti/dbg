395da0c77d11475c50848ffafb53d4dddfed0200
Johannes Marti <johannes.marti@gmail.com>
2022-10-14 00:04:01 +0100

    introducing Bitable

---
+++ src/Bitable.hs
@@ -0,0 +1,58 @@
+module Bitable (
+  Bitification(..),
+  BitableI,
+) where
+
+import Data.Set as Set
+
+import Bitify
+import BitGraph
+import Coding
+import ConciseGraph
+import CommonLGraphTypes
+import Graph
+import LiftedGraph
+import qualified LabeledGraph as LG
+import RelationTree
+import WordTree (labelOfWord)
+import LWrappedGraph
+
+data Bitification x = Bitification {
+  size :: Size,
+  labeledBitGraph :: LBitGraph,
+  reflexivesUniversalInMultiple :: BitGraph -> Set.Set x,
+  relationI :: GraphI BitGraph x
+}
+
+type BitableI g x = g -> Bitification x
+
+lBitGraphBitableI :: Size -> BitableI LBitGraph Node
+lBitGraphBitableI s lbg =
+  Bitification s lbg (reflexivesUnivInMultiple s) (bitGraphI s)
+
+conciseGraphBitableI :: Size -> BitableI ConciseGraph Node
+conciseGraphBitableI s cg = bitification where
+  lbg = ConciseGraph.toLBitGraph s cg
+  bitification = lBitGraphBitableI s lbg
+
+liftedGraphBitableI :: BitableI (LiftedGraph x) Int
+liftedGraphBitableI lg = bitification where
+  (lbg,s) = LiftedGraph.toLBitGraph lg
+  bitification = lBitGraphBitableI s lbg
+
+genericBitableI :: Ord x => LG.LabeledGraphI g x -> BitableI g x
+genericBitableI gi g = bitification where
+  (wg,s) = labeledBitify gi g
+  c = coding wg
+  decodePair (x,y) = (decode c x, decode c y)
+  bgi = bitGraphI s
+  rum bg = Set.map (decode c) $ reflexivesUnivInMultiple s bg
+  outTypeI = interfaceFromAll (\_ -> LG.domain (lWrappedGraphI (lBitGraphI s)) wg)
+                              (\bg node -> Set.map (decode c) $
+                                            successors bgi bg (encode c node))
+                              (\bg node -> Set.map (decode c) $
+                                            predecessors bgi bg (encode c node))
+                              (\bg (x,y) -> hasArc bgi bg (encode c x, encode c y))
+                              (\bg -> Prelude.map decodePair $ arcs bgi bg)
+                              (\bg node -> prettyOuterNode wg node)
+  bitification = Bitification s (innerGraph wg) rum outTypeI
--- src/RelationCache.hs
+++ src/RelationCache.hs
@@ -2,30 +2,20 @@ module RelationCache (
   RelationCachableI,
   RelationCache,
   outputType,
-  reflexivesUniversalInMultiple,
+  RelationCache.reflexivesUniversalInMultiple,
   relationOfWord,
   buildCache,
-  lBitGraphRelationCacheI,
-  liftedGraphRelationCacheI,
-  genericRelationCacheI
+  relationTreeRelationCacheableI,
 ) where
 
-import Data.Set as Set
+import Data.Set
 
-import Bitify
+import Bitable
 import BitGraph
-import Coding
-import ConciseGraph
-import CommonLGraphTypes
 import Graph
 import Label
-import qualified LabeledGraph as LG
-import LiftedGraph
 import RelationTree
 import WordTree (labelOfWord)
-import LWrappedGraph
-
-import Data.Set
 
 data RelationCache r x = RelationCache {
   outputType     :: GraphI r x,
@@ -33,42 +23,13 @@ data RelationCache r x = RelationCache {
   relationOfWord :: [Label] -> r
 }
 
-type RelationCachableI g x r = g -> (RelationCache r x)
+type RelationCachableI g x r = g -> RelationCache r x
 
 buildCache :: RelationCachableI g x r -> g -> RelationCache r x
 buildCache ci = ci
 
-lBitGraphRelationCacheI :: Size -> RelationCachableI LBitGraph Node BitGraph
-lBitGraphRelationCacheI s lbg = RelationCache (bitGraphI s) rum rw where
-  rum = reflexivesUnivInMultiple s
+relationTreeRelationCacheableI :: BitableI g x -> RelationCachableI g x BitGraph
+relationTreeRelationCacheableI bi g = RelationCache bgi rum rw where
+  Bitification s lbg rum bgi = bi g
   rt = relationTree (lbg,s)
   rw = labelOfWord rt
-
-liftedGraphRelationCacheI :: RelationCachableI (LiftedGraph x) Int BitGraph
-liftedGraphRelationCacheI lg = cache where
-  (lbg,s) = LiftedGraph.toLBitGraph lg
-  cache = buildCache (lBitGraphRelationCacheI s) lbg
-
-conciseGraphRelationCacheI :: Size -> RelationCachableI
-                                        ConciseGraph Node BitGraph
-conciseGraphRelationCacheI s cg = cache where
-  lbg = ConciseGraph.toLBitGraph s cg
-  cache = buildCache (lBitGraphRelationCacheI s) lbg
-
-genericRelationCacheI :: Ord x => LG.LabeledGraphI g x -> RelationCachableI g x BitGraph
-genericRelationCacheI gi g = cache where
-  (wg,s) = labeledBitify gi g
-  c = coding wg
-  decodePair (x,y) = (decode c x, decode c y)
-  bgCache = lBitGraphRelationCacheI s (innerGraph wg)
-  bgi = bitGraphI s
-  rum bg = Set.map (decode c) $ reflexivesUniversalInMultiple bgCache bg
-  outTypeI = interfaceFromAll (\_ -> LG.domain (lWrappedGraphI (lBitGraphI s)) wg)
-                              (\bg node -> Set.map (decode c) $
-                                            successors bgi bg (encode c node))
-                              (\bg node -> Set.map (decode c) $
-                                            predecessors bgi bg (encode c node))
-                              (\bg (x,y) -> hasArc bgi bg (encode c x, encode c y))
-                              (\bg -> Prelude.map decodePair $ arcs bgi bg)
-                              (\bg node -> prettyOuterNode wg node)
-  cache = RelationCache outTypeI rum (relationOfWord bgCache)
