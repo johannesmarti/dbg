1750b4c0beaa3cd91e656e621a740eb582c43e55
Johannes Marti <johannes.marti@gmail.com>
2023-02-27 21:28:54 +0000

    restructuring

---
+++ app/address/Main.hs
@@ -0,0 +1,25 @@
+module Main (
+  main
+) where
+
+import FlyingPig
+import Label
+import Word
+
+a :: [Label]
+a = [Zero,One,One,One]
+b = a ++ [Zero,One,Zero,Zero,Zero] ++ a ++ [Zero,One,One]
+
+addressPrinter :: PigNode -> [String]
+addressPrinter node = 
+  if node == epsilon then []
+  else (prettyWord (turningWord node) ++ " at address "
+            ++ prettyWord (address node)) : addressPrinter (parent node)
+
+main :: IO ()
+main = let
+    p0 = predecessor Zero
+    p1 = predecessor One
+    --node = p0 . p1 . p0 . p0 . p1 . p0 . p1 . p0 . p0 $ one
+    node = lookupAddress b
+  in mapM_ putStrLn (addressPrinter node)
--- app/report/Main.hs
+++ app/report/Main.hs
@@ -19,13 +19,14 @@ import qualified SmartSearch as SS
 main :: IO ()
 
 --(gi, g) = (dbgI,dbg 4)
-(gi, g) = (alloc3I,alloc3)
+--(gi, g) = (alloc3I,alloc3)
 --(gi, g) = (big5I,big5)
 --(gi, g) = (force9dI,force9d)
 --(gi, g) = (force3dI,force3d)
 --(gi, g) = (b1ef5I,b1ef5)
 --(gi, g) = (specialUnfoldI,specialUnfold)
 --(gi, g) = (biggestI,biggest)
+(gi, g) = (hamburgerI,hamburger)
 pgi = powerGraphI gi
 main = do
   putStr . unlines $ prettyLabeledGraph gi g
