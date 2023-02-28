2d01f8fb7d1cc76c91c10d035ca8d48d095eedaf
Johannes Marti <johannes.marti@gmail.com>
2023-02-28 11:54:30 +0000

    adapting some of the output tools

---
+++ app/cayrep/Main.hs
@@ -0,0 +1,31 @@
+module Main (
+  main
+) where
+
+import System.Environment 
+import Data.Set as Set
+
+import Report
+import Patterns
+import Label
+import LabeledGraph
+import DeBruijnGraph
+
+
+import qualified SmartSearch as SS
+
+
+main :: IO ()
+
+--(gi, g) = (dbgI,dbg 4)
+--(gi, g) = (alloc3I,alloc3)
+--(gi, g) = (big5I,big5)
+--(gi, g) = (force9dI,force9d)
+(gi, g) = (force3dI,force3d)
+--(gi, g) = (b1ef5I,b1ef5)
+--(gi, g) = (specialUnfoldI,specialUnfold)
+--(gi, g) = (biggestI,biggest)
+--(gi, g) = (hamburgerI,hamburger)
+main = do
+  easyCayleyReport gi g
+
