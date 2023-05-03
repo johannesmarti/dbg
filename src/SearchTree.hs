1ae46c18bb554274fa3bf4989e21ad2367925e01
Johannes Marti <johannes.marti@gmail.com>
2023-05-03 12:45:12 +0100

    implemented variants of tree search that return lists

--- src/SearchTree.hs
+++ src/SearchTree.hs
@@ -3,9 +3,13 @@ module SearchTree (
   SearchTree(SearchTreeNode),
   searchTreeFromGenerator,
   hotPathSearch,
+  hotPathSearch',
   depthFirstSearch,
+  depthFirstSearch',
   breadthFirstSearch,
+  breadthFirstSearch',
   dovetailingSearch,
+  dovetailingSearch',
 ) where
 
 import Data.List.Extra (firstJust)
@@ -28,34 +32,56 @@ searchTreeFromGenerator generator = worker (generateRoot generator) where
 Searches just along the left most path, that is the path that always follows
 the first child. Any subtrees of younger siblings are not searched.
 -}
-hotPathSearch :: (d -> Bool) -> Int -> SearchTree d -> Maybe d
-hotPathSearch predicate depth tree = let dl = label tree
+hotPathSearch :: (d -> Bool) -> Int -> SearchTree d -> [d]
+hotPathSearch predicate depth tree = if predicate dl then dl : t else t where
+  dl = label tree
+  t = if depth <= 0 then []
+      else case children tree of
+             []    -> []
+             (h:_) -> hotPathSearch predicate (depth - 1) h
+
+hotPathSearch' :: (d -> Bool) -> Int -> SearchTree d -> Maybe d
+hotPathSearch' predicate depth tree = let dl = label tree
   in if predicate dl then Just dl
      else if depth <= 0 then Nothing
      else case children tree of
             []    -> Nothing
-            (h:_) -> hotPathSearch predicate (depth - 1) h
+            (h:_) -> hotPathSearch' predicate (depth - 1) h
 
-depthFirstSearch :: (d -> Bool) -> Int -> SearchTree d -> Maybe d
-depthFirstSearch predicate depth tree = let dl = label tree
+depthFirstSearch :: (d -> Bool) -> Int -> SearchTree d -> [d]
+depthFirstSearch predicate depth tree = if predicate dl then dl : t else t where
+  dl = label tree
+  t = if depth <= 0 then []
+      else concatMap (depthFirstSearch predicate (depth - 1)) (children tree)
+
+depthFirstSearch' :: (d -> Bool) -> Int -> SearchTree d -> Maybe d
+depthFirstSearch' predicate depth tree = let dl = label tree
   in if predicate dl then Just dl
      else if depth <= 0 then Nothing
-     else firstJust (depthFirstSearch predicate (depth - 1)) (children tree)
+     else firstJust (depthFirstSearch' predicate (depth - 1)) (children tree)
 
 {-
 It switches the order at every level. Thus in a binary tree we search in this
 order r,0,1,10,11,00,01,010,011,001,001,110,111,100,101,1010,1011,1000,...
 This is done to avoid reversing and appending at the end of worklists.
 -}
-breadthFirstSearch :: (d -> Bool) -> Int -> SearchTree d -> Maybe d
+breadthFirstSearch :: (d -> Bool) -> Int -> SearchTree d -> [d]
 breadthFirstSearch predicate depth tree = worker depth [tree] [] where
+  worker _ [] [] = []
+  worker d [] nextLevel = if d <= 0 then []
+                          else worker (d - 1) nextLevel []
+  worker d (next:rest) nextLevel = if predicate dl then dl : t else t where
+    dl = label next
+    t  = worker d rest ((children next) ++ nextLevel)
+
+breadthFirstSearch' :: (d -> Bool) -> Int -> SearchTree d -> Maybe d
+breadthFirstSearch' predicate depth tree = worker depth [tree] [] where
   worker _ [] [] = Nothing
   worker d [] nextLevel = if d <= 0 then Nothing
                           else worker (d - 1) nextLevel []
   worker d (next:rest) nextLevel = let dl = label next
     in if predicate dl then Just dl
        else worker d rest ((children next) ++ nextLevel)
-
 {-
 Searches a tree
   (r,[(0, [(00,[...]), ...]),(1, [(10, [...]), ...]), (2, [...]), ...])
@@ -64,8 +90,17 @@ Is implemented with a worklist. In every step we are adding elements at the end
 of the list. It would probabely be better to use some specialized fifo
 datastructure as the worklist. Steps is the number of labels that is checked. A negative number leads to an infinte search.
 -}
-dovetailingSearch :: (d -> Bool) -> Int -> SearchTree d -> Maybe d
+dovetailingSearch :: (d -> Bool) -> Int -> SearchTree d -> [d]
 dovetailingSearch predicate steps tree = worker steps [[tree]] where
+  worker 0 _ = []
+  worker _ [] = []
+  worker s ([]:rest) = worker s rest
+  worker s ((next:siblings):rest) = if predicate dl then dl : t else t where
+    dl = label next
+    t = worker (s - 1) (rest ++ [children next, siblings])
+
+dovetailingSearch' :: (d -> Bool) -> Int -> SearchTree d -> Maybe d
+dovetailingSearch' predicate steps tree = worker steps [[tree]] where
   worker 0 _ = Nothing
   worker _ [] = Nothing
   worker s ([]:rest) = worker s rest
--- test/SearchTreeSpec.hs
+++ test/SearchTreeSpec.hs
@@ -13,45 +13,89 @@ randomTree = n 1 [n 11 [n 111 [n 1111 [], n 1113 [n 4 []]], n 113 []],
 
 spec :: Spec
 spec = do
-  describe "applying hotPathSearch to randomTree" $ do
+  describe "hotPathSearch in randomTree" $ do
     it "don't find an even on hot path" $
-      hotPathSearch even 100 randomTree `shouldBe` Nothing
+      hotPathSearch even 100 randomTree `shouldBe` []
     it "find 111" $
-      hotPathSearch (== 111) 100 randomTree `shouldBe` Just 111
+      hotPathSearch (== 111) 100 randomTree `shouldBe` [111]
+    it "find 1 and 11 for predicate <100" $
+      hotPathSearch (\n -> n < 100) 100 randomTree `shouldBe` [1,11]
     it "don't find 1111 if I only search to depth 2" $
-      hotPathSearch (== 1111) 2 randomTree `shouldBe` Nothing
-  describe "applying depthFirstSearch to randomTree" $ do
-    it "find 4 as even" $
-      depthFirstSearch even 100 randomTree `shouldBe` Just 4
+      hotPathSearch (== 1111) 2 randomTree `shouldBe` []
+  describe "hotPathSearch' in randomTree" $ do
+    it "don't find an even on hot path" $
+      hotPathSearch' even 100 randomTree `shouldBe` Nothing
+    it "find 111" $
+      hotPathSearch' (== 111) 100 randomTree `shouldBe` Just 111
+    it "don't find 1111 if I only search to depth 2" $
+      hotPathSearch' (== 1111) 2 randomTree `shouldBe` Nothing
+  describe "depthFirstSearch in randomTree" $ do
+    it "find 4 and 2 as even" $
+      depthFirstSearch even 100 randomTree `shouldBe` [4,2]
     it "don't find 42" $
-      depthFirstSearch (== 42) 100 randomTree `shouldBe` Nothing
+      depthFirstSearch (== 42) 100 randomTree `shouldBe` []
     it "don't find an even if I only search to depth 1" $
-      depthFirstSearch even 1 randomTree `shouldBe` Nothing
+      depthFirstSearch even 1 randomTree `shouldBe` []
     it "find even 2 if I search to depth 2" $
-      depthFirstSearch even 2 randomTree `shouldBe` Just 2
-  describe "applying breadthFirstSearch to randomTree" $ do
-    it "find 1" $
-      breadthFirstSearch (== 1) 10 randomTree `shouldBe` Just 1
-    it "find 11" $
-      breadthFirstSearch (== 11) 10 randomTree `shouldBe` Just 11
-    it "find 13" $
-      breadthFirstSearch (== 13) 10 randomTree `shouldBe` Just 13
-    it "find 1113" $
-      breadthFirstSearch (== 1113) 10 randomTree `shouldBe` Just 1113
-    it "find 4" $
-      breadthFirstSearch (== 4) 10 randomTree `shouldBe` Just 4
-    it "find 2 as even" $
-      breadthFirstSearch even 10 randomTree `shouldBe` Just 2
-    it "don't find 42" $
-      breadthFirstSearch (== 42) 10 randomTree `shouldBe` Nothing
-    it "don't find an even if I only search to breadth 1" $
-      breadthFirstSearch even 1 randomTree `shouldBe` Nothing
-    it "find even 2 if I search to breadth 2" $
-      breadthFirstSearch even 2 randomTree `shouldBe` Just 2
-  describe "applying dovetailingSearch to randomTree" $ do
+      depthFirstSearch even 2 randomTree `shouldBe` [2]
+  describe "depthFirstSearch' in randomTree" $ do
     it "find 4 as even" $
-      dovetailingSearch even 11 randomTree `shouldBe` Just 4
+      depthFirstSearch' even 100 randomTree `shouldBe` Just 4
     it "don't find 42" $
-      dovetailingSearch (== 42) 10 randomTree `shouldBe` Nothing
+      depthFirstSearch' (== 42) 100 randomTree `shouldBe` Nothing
+    it "don't find an even if I only search to depth 1" $
+      depthFirstSearch' even 1 randomTree `shouldBe` Nothing
+    it "find even 2 if I search to depth 2" $
+      depthFirstSearch' even 2 randomTree `shouldBe` Just 2
+  describe "breadthFirstSearch in randomTree" $ do
+    it "find 1" $
+      breadthFirstSearch (== 1) 10 randomTree `shouldBe` [1]
+    it "find 11" $
+      breadthFirstSearch (== 11) 10 randomTree `shouldBe` [11]
+    it "find 13" $
+      breadthFirstSearch (== 13) 10 randomTree `shouldBe` [13]
+    it "find 1113" $
+      breadthFirstSearch (== 1113) 10 randomTree `shouldBe` [1113]
+    it "find 4" $
+      breadthFirstSearch (== 4) 10 randomTree `shouldBe` [4]
+    it "find 2 and 4 as even" $
+      breadthFirstSearch even 10 randomTree `shouldBe` [2,4]
+    it "don't find 42" $
+      breadthFirstSearch (== 42) 10 randomTree `shouldBe` []
+    it "don't find an even if I only search to breadth 1" $
+      breadthFirstSearch even 1 randomTree `shouldBe` []
+    it "find even 2 if I search to breadth 2" $
+      breadthFirstSearch even 2 randomTree `shouldBe` [2]
+  describe "breadthFirstSearch' in randomTree" $ do
+    it "find 1" $
+      breadthFirstSearch' (== 1) 10 randomTree `shouldBe` Just 1
+    it "find 11" $
+      breadthFirstSearch' (== 11) 10 randomTree `shouldBe` Just 11
+    it "find 13" $
+      breadthFirstSearch' (== 13) 10 randomTree `shouldBe` Just 13
+    it "find 1113" $
+      breadthFirstSearch' (== 1113) 10 randomTree `shouldBe` Just 1113
+    it "find 4" $
+      breadthFirstSearch' (== 4) 10 randomTree `shouldBe` Just 4
+    it "find 2 as even" $
+      breadthFirstSearch' even 10 randomTree `shouldBe` Just 2
+    it "don't find 42" $
+      breadthFirstSearch' (== 42) 10 randomTree `shouldBe` Nothing
+    it "don't find an even if I only search to breadth 1" $
+      breadthFirstSearch' even 1 randomTree `shouldBe` Nothing
+    it "find even 2 if I search to breadth 2" $
+      breadthFirstSearch' even 2 randomTree `shouldBe` Just 2
+  describe "dovetailingSearch in randomTree" $ do
+    it "find 4 and 2 as even" $
+      dovetailingSearch even 1000 randomTree `shouldBe` [4,2]
+    it "don't find 42" $
+      dovetailingSearch (== 42) 10 randomTree `shouldBe` []
     it "don't find an even if I only search 5 steps" $
-      dovetailingSearch even 5 randomTree `shouldBe` Nothing
+      dovetailingSearch even 5 randomTree `shouldBe` []
+  describe "dovetailingSearch' in randomTree" $ do
+    it "find 4 as even" $
+      dovetailingSearch' even 11 randomTree `shouldBe` Just 4
+    it "don't find 42" $
+      dovetailingSearch' (== 42) 10 randomTree `shouldBe` Nothing
+    it "don't find an even if I only search 5 steps" $
+      dovetailingSearch' even 5 randomTree `shouldBe` Nothing
