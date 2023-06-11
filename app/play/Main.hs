ee9f149a4bd34fc5a04ea3ba00957306e8aa1700
Johannes Marti <johannes.marti@gmail.com>
2023-06-11 17:05:05 +0200

    found cycles with 3 and more ascend points

--- app/play/Main.hs
+++ app/play/Main.hs
@@ -12,8 +12,8 @@ import Path (labelList)
 addressPrinter :: CoveringNode -> String
 addressPrinter node = 
   (prettyWord (turningWord node) ++ " at address "
-            ++ prettyWord (address node)) ++ " with path down "
-            ++ prettyWord (reverse . labelList . fullPathDown $ node)
+            ++ prettyWord (address node)) ++ " is ascending: "
+            ++ show (isAscending node)
 
 listPrinter :: [CoveringNode] -> IO ()
 listPrinter [] = putStrLn "===="
@@ -28,5 +28,8 @@ main = do
   args <- getArgs
   let numNodes = read (head args)
   let nfs = take numNodes cycles
-  let hello = concatMap (filter isAscending . cycleOfNode) nfs
-  listPrinter hello
+  let cycs = map cycleOfNode nfs
+  let isInteresting cycle = length (filter isAscending cycle) > 2
+  let wow = filter isInteresting cycs
+  putStrLn (show (length wow))
+  listPrinter (head wow)
